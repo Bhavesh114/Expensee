986cfc350a9c750757f2be05eebf514635813c86
divya2399 <53622635+divya2399@users.noreply.github.com>
2020-07-22 18:56:12 +0530

    Updated Splash and Onboarding

--- android/app/src/main/java/com/bhavesh/expensee/MainActivity.java
+++ android/app/src/main/java/com/bhavesh/expensee/MainActivity.java
@@ -1,6 +1,14 @@
 package com.bhavesh.expensee;
 
-import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
 }
--- lib/LandingPage.dart
+++ lib/LandingPage.dart
@@ -1,18 +1,40 @@
+import 'Screens/Login_Screen.dart';
 import 'Screens/Onboarding_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:expensee/models/user.dart';
 import 'package:expensee/Screens/home.dart';
+import 'package:after_layout/after_layout.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
 
-import 'Screens/Onboarding_Screen.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> with AfterLayoutMixin<LandingPage>{
+  Future checkFirstSeen() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen')??false);
+
+    if (_seen){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> Loginscreen()));
+    }else{
+      await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>OnboardingScreen()));
+    }
+  }
+  @override
+  void afterFirstLayout(BuildContext context) => checkFirstSeen();
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
 
     if (user == null)
-      return OnboardingScreen();
+      return Loginscreen();
     else
       return HomePage();
   }
