212826498c48064d0767708f77728c60b58a551b
Bhavesh Singhal <bhaveshsinghal114@gmail.com>
2020-07-28 03:50:44 +0530

    Bottom bar and working calendar

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 const TextStyle kInputLoginHIntText = TextStyle(
   fontFamily: 'GT Walsheim Pro',
@@ -40,4 +41,30 @@ const kCalendarTextStyle = TextStyle(
   color: Colors.black,
   fontSize: 20.0,
   fontWeight: FontWeight.bold,
+);
+
+const kCalendarStyle = CalendarStyle(
+    selectedColor: Color(0xffFF3378),
+    todayColor: Color(0xfffcd4e2),
+    todayStyle: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 16.0,
+      color: Colors.white,
+    ));
+
+const kboxDecoration = BoxDecoration(
+  borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(23.98),
+    topRight: Radius.circular(23.98),
+    bottomRight: Radius.circular(35.97),
+    bottomLeft: Radius.circular(35.97),
+  ),
+  color: const Color(0xffffffff),
+  boxShadow: [
+    BoxShadow(
+      color: const Color(0xffe6e3e3),
+      offset: Offset(0, 8),
+      blurRadius: 24,
+    ),
+  ],
 );
--- lib/Screens/DailyEmpty.dart
+++ lib/Screens/DailyEmpty.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:expensee/Constants.dart';
+import 'package:navigation_action_bar/navigation_action_bar.dart';
+
+import '../Constants.dart';
 
 class EmptyStateDaily extends StatefulWidget {
   @override
@@ -9,12 +11,17 @@ class EmptyStateDaily extends StatefulWidget {
 
 class _EmptyStateDailyState extends State<EmptyStateDaily> {
   CalendarController _controller;
-  //HeaderGestureCallback ontapheader;
-
+  double currentIndex = 0.0;
   @override
   void initState() {
     super.initState();
     _controller = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -23,94 +30,71 @@ class _EmptyStateDailyState extends State<EmptyStateDaily> {
     queryData = MediaQuery.of(context);
 
     return Scaffold(
-      backgroundColor: Color(0xffFCFCFC),
-//      appBar: AppBar(
-//        backgroundColor: Colors.white,
-//        title: Text('Daily Transaction', style: TextStyle(
-//          fontSize: 20.0,
-//          fontWeight: FontWeight.bold,
-//          color: Colors.black,
-//        ),),
-//      ),
-      body:
-      Column(
-        children: <Widget>[
-          Container(
-            child: SingleChildScrollView(
-              child: TableCalendar(
-                initialCalendarFormat: CalendarFormat.week,
-                calendarStyle: CalendarStyle(
-                  selectedColor: Color(0xffFF3378),
-                  todayColor: Color(0xfffcd4e2),
-                  todayStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-                //onHeaderTapped: ontapheader, need to figure out this
-                headerStyle: HeaderStyle(
-                  centerHeaderTitle: false,
-                  formatButtonDecoration: BoxDecoration(
-                    // color: Color(0xffFF3300),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  formatButtonTextStyle: TextStyle(color: Colors.black),
-                  formatButtonShowsNext: false,
-                ),
-                startingDayOfWeek: StartingDayOfWeek.monday,
-                onDaySelected: (date, events) {
-                  print(date.toIso8601String());
-                },
-                builders: CalendarBuilders(
-                  selectedDayBuilder: (context, date, events) => Container(
-                    margin: const EdgeInsets.all(4.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Color(0xffFF3378),
-                      borderRadius: BorderRadius.circular(30.0),
+        backgroundColor: Color(0xffFCFCFC),
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: SingleChildScrollView(
+                child: TableCalendar(
+                  initialCalendarFormat: CalendarFormat.week,
+                  calendarStyle: kCalendarStyle,
+                  headerStyle: HeaderStyle(
+                    centerHeaderTitle: false,
+                    formatButtonDecoration: BoxDecoration(
+                      // color: Color(0xffFF3300),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    child: Text(
-                      date.day.toString(),
-                      style: TextStyle(
-                        color: Colors.white,
+                    formatButtonTextStyle: TextStyle(color: Colors.black),
+                    formatButtonShowsNext: false,
+                  ),
+                  startingDayOfWeek: StartingDayOfWeek.monday,
+                  onDaySelected: (date, events) {
+                    print(date.toIso8601String());
+                  },
+                  builders: CalendarBuilders(
+                    selectedDayBuilder: (context, date, events) => Container(
+                      margin: const EdgeInsets.all(4.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Color(0xffFF3378),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
+                  calendarController: _controller,
                 ),
-                calendarController: _controller,
               ),
-            ),
-            width: queryData.size.width,//380.7,
-            height: 185.9,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(23.98),
-                topRight: Radius.circular(23.98),
-                bottomRight: Radius.circular(35.97),
-                bottomLeft: Radius.circular(35.97),
-              ),
-              color: const Color(0xffffffff),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0xffe6e3e3),
-                  offset: Offset(0, 8),
-                  blurRadius: 24,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Image.asset(
-              "images/DailyEmpty.png",
-              height: 250.0,
-              width: 250.0,
-            ),
-          ),
-        ],
-      ),
-
-    );
+              decoration: kboxDecoration,
+            ), //Table Calendar
+          ],
+        ),
+        bottomNavigationBar: NavigationActionBar(
+          context: context,
+          scaffoldColor: Color(0xffff3378),
+          index: 0,
+          subItems: [
+            NavBarItem(iconData: Icons.arrow_downward, size: 25),
+            NavBarItem(iconData: Icons.arrow_upward, size: 25),
+          ],
+          mainIndex: 2,
+          items: [
+            NavBarItem(iconData: Icons.calendar_today, size: 30),
+            NavBarItem(iconData: Icons.insert_chart, size: 30),
+            NavBarItem(iconData: Icons.add, size: 40),
+            NavBarItem(iconData: Icons.account_balance_wallet, size: 30),
+            NavBarItem(iconData: Icons.person_outline, size: 30),
+          ],
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ));
   }
 }
