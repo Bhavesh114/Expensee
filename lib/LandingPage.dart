07bd4c2b4d51c2e58d2f3745b36192cba11fd8f4
Bhavesh Singhal <bhaveshsinghal114@gmail.com>
2020-07-28 02:41:51 +0530

    m

--- lib/LandingPage.dart
+++ lib/LandingPage.dart
@@ -1,31 +1,34 @@
+import 'Screens/DailyEmpty.dart';
 import 'Screens/Login_Screen.dart';
 import 'Screens/Onboarding_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:expensee/models/user.dart';
-import 'package:expensee/Screens/home.dart';
 import 'package:after_layout/after_layout.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
-
 
 class LandingPage extends StatefulWidget {
   @override
   _LandingPageState createState() => _LandingPageState();
 }
 
-class _LandingPageState extends State<LandingPage> with AfterLayoutMixin<LandingPage>{
-  Future checkFirstSeen() async{
+class _LandingPageState extends State<LandingPage>
+    with AfterLayoutMixin<LandingPage> {
+  Future checkFirstSeen() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool _seen = (prefs.getBool('seen')??false);
+    bool _seen = (prefs.getBool('seen') ?? false);
 
-    if (_seen){
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> Loginscreen()));
-    }else{
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => Loginscreen()));
+    } else {
       await prefs.setBool('seen', true);
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>OnboardingScreen()));
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => OnboardingScreen()));
     }
   }
+
   @override
   void afterFirstLayout(BuildContext context) => checkFirstSeen();
 
@@ -36,6 +39,6 @@ class _LandingPageState extends State<LandingPage> with AfterLayoutMixin<Landing
     if (user == null)
       return Loginscreen();
     else
-      return HomePage();
+      return EmptyStateDaily();
   }
 }
