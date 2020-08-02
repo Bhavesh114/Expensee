d419930a586dcae5c9e8b345cf55368501b3dba2
divya2399 <53622635+divya2399@users.noreply.github.com>
2020-08-02 09:13:10 +0530

    After Login Pages

---
+++ lib/Screens/Add.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Add extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('FILE SCREEN'),
+      backgroundColor: Colors.black12,
+    );
+  }
+}
---
+++ lib/Screens/Profile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('FILE SCREEN'),
+      backgroundColor: Colors.black12,
+    );
+  }
+}
