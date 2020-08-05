32624d2cd2cb718ad79bfc6e879de0d87ccb8f40
Bhavesh Singhal <bhaveshsinghal114@gmail.com>
2020-08-06 01:04:59 +0530

    Working on Add screen, Stats calendar is still an issue.

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -28,6 +28,8 @@ class _LoginscreenState extends State<Loginscreen>
       body: ModalProgressHUD(
         inAsyncCall: spinner,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
               width: MediaQuery.of(context).size.width,
@@ -98,13 +100,18 @@ class _LoginscreenState extends State<Loginscreen>
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0),
+            SizedBox(
+              height: 15.0,
+            ),
+            Center(
               child: Image.asset(
                 "images/loginImage.png",
                 height: 200.0,
                 width: 200.0,
               ),
+            ),
+            SizedBox(
+              height: 15.0,
             ),
             Center(
               child: Text(
@@ -121,25 +128,20 @@ class _LoginscreenState extends State<Loginscreen>
             SizedBox(
               height: 10.0,
             ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 19.0),
-                  child: Text(
-                    'Email',
-                    style: TextStyle(
-                      fontFamily: 'GT Walsheim Pro',
-                      fontSize: 12,
-                      color: const Color(0xffaeb1b8),
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
+            Padding(
+              padding: const EdgeInsets.only(left: 19.0),
+              child: Text(
+                'Email',
+                style: TextStyle(
+                  fontFamily: 'GT Walsheim Pro',
+                  fontSize: 12,
+                  color: const Color(0xffaeb1b8),
                 ),
-              ],
+                textAlign: TextAlign.left,
+              ),
             ),
             Padding(
-              padding:
-                  const EdgeInsets.only(right: 35.0, left: 19.0, bottom: 0.0),
+              padding: const EdgeInsets.only(right: 35.0, left: 19.0),
               child: TextField(
                 keyboardType: TextInputType.emailAddress,
                 onChanged: (value) {
@@ -236,8 +238,10 @@ class _LoginscreenState extends State<Loginscreen>
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+            SizedBox(
+              height: 15.0,
+            ),
+            Center(
               child: SizedBox(
                 width: 18.0,
                 child: Text(
@@ -251,74 +255,81 @@ class _LoginscreenState extends State<Loginscreen>
                 ),
               ),
             ),
-            FlatButton(
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              onPressed: () {
-                print("Login with FaceBook");
-              },
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    width: 300.0,
-                    height: 48.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(
-                          width: 1.0, color: const Color(0xff3b5998)),
-                    ),
-                  ),
-                  Transform.translate(
-                    offset: Offset(17.54, 17.67),
-                    child:
-                        // Adobe XD layer: 'Button text' (text)
-                        SizedBox(
-                      width: 266.0,
-                      height: 50.0,
-                      child: Text(
-                        'Login with facebook',
-                        style: TextStyle(
-                          fontFamily: 'GT Walsheim Pro',
-                          fontSize: 15,
-                          color: const Color(0xff3b5998),
-                          fontWeight: FontWeight.w500,
-                          height: 0.9,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  Transform.translate(
-                    offset: Offset(23.08, 16.0),
-                    child:
-                        // Adobe XD layer: 'Shape' (shape)
-                        SvgPicture.string(
-                      '<svg viewBox="23.1 16.0 7.4 17.0" ><path transform="translate(23.08, 16.0)" d="M 4.910446643829346 17 L 4.910446643829346 8.499004364013672 L 7.095101356506348 8.499004364013672 L 7.384615421295166 5.569514751434326 L 4.910446643829346 5.569514751434326 L 4.914158344268799 4.10327672958374 C 4.914158344268799 3.339220523834229 4.981742858886719 2.929821729660034 6.003392696380615 2.929821729660034 L 7.369149684906006 2.929821729660034 L 7.369149684906006 0 L 5.184185981750488 0 C 2.559692859649658 0 1.635939240455627 1.421117901802063 1.635939240455627 3.810991287231445 L 1.635939240455627 5.569846630096436 L 0 5.569846630096436 L 0 8.499336242675781 L 1.635939240455627 8.499336242675781 L 1.635939240455627 17 L 4.910446643829346 17 Z" fill="#3b5998" stroke="none" stroke-width="1" stroke-miterlimit="10" stroke-linecap="butt" /></svg>',
-                      allowDrawingOutsideViewBox: true,
-                    ),
-                  ),
-                ],
-              ),
+            SizedBox(
+              height: 15.0,
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0),
+            Center(
               child: FlatButton(
                 splashColor: Colors.transparent,
                 highlightColor: Colors.transparent,
                 onPressed: () {
-                  print("Forgot Pasword");
+                  print("Login with FaceBook");
                 },
-                child: SizedBox(
-                  width: 112.0,
-                  child: Text(
-                    'Forgot password?',
-                    style: TextStyle(
-                      fontFamily: 'GT Walsheim Pro',
-                      fontSize: 12,
-                      fontWeight: FontWeight.w300,
-                      color: const Color(0xff1c202e),
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      width: 300.0,
+                      height: 48.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                            width: 1.0, color: const Color(0xff3b5998)),
+                      ),
                     ),
-                    textAlign: TextAlign.center,
+                    Transform.translate(
+                      offset: Offset(17.54, 17.67),
+                      child:
+                          // Adobe XD layer: 'Button text' (text)
+                          SizedBox(
+                        width: 266.0,
+                        height: 50.0,
+                        child: Text(
+                          'Login with facebook',
+                          style: TextStyle(
+                            fontFamily: 'GT Walsheim Pro',
+                            fontSize: 15,
+                            color: const Color(0xff3b5998),
+                            fontWeight: FontWeight.w500,
+                            height: 0.9,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Transform.translate(
+                      offset: Offset(23.08, 16.0),
+                      child:
+                          // Adobe XD layer: 'Shape' (shape)
+                          SvgPicture.string(
+                        '<svg viewBox="23.1 16.0 7.4 17.0" ><path transform="translate(23.08, 16.0)" d="M 4.910446643829346 17 L 4.910446643829346 8.499004364013672 L 7.095101356506348 8.499004364013672 L 7.384615421295166 5.569514751434326 L 4.910446643829346 5.569514751434326 L 4.914158344268799 4.10327672958374 C 4.914158344268799 3.339220523834229 4.981742858886719 2.929821729660034 6.003392696380615 2.929821729660034 L 7.369149684906006 2.929821729660034 L 7.369149684906006 0 L 5.184185981750488 0 C 2.559692859649658 0 1.635939240455627 1.421117901802063 1.635939240455627 3.810991287231445 L 1.635939240455627 5.569846630096436 L 0 5.569846630096436 L 0 8.499336242675781 L 1.635939240455627 8.499336242675781 L 1.635939240455627 17 L 4.910446643829346 17 Z" fill="#3b5998" stroke="none" stroke-width="1" stroke-miterlimit="10" stroke-linecap="butt" /></svg>',
+                        allowDrawingOutsideViewBox: true,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: FlatButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    print("Forgot Pasword");
+                  },
+                  child: SizedBox(
+                    width: 112.0,
+                    child: Text(
+                      'Forgot password?',
+                      style: TextStyle(
+                        fontFamily: 'GT Walsheim Pro',
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        color: const Color(0xff1c202e),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
               ),
